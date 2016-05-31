94e953d82b9e0eed9b4323832177477646a221cc
Hendrik Purmann <h.purmann@googlemail.com>
2016-05-31 14:37:30 +0200

    Add actano's eslint configuration

--- bot/scripts/lunch.js
+++ bot/scripts/lunch.js
@@ -1,56 +1,58 @@
 module.exports = (robot) => {
-
-  let choices = {
-    0: "Not hungry",
-    1: "Saigon",
-    2: "Italian",
-    3: "Austrian",
-    4: "Burger",
-    5: "Falafel",
+  const choices = {
+    0: 'Not hungry',
+    1: 'Saigon',
+    2: 'Italian',
+    3: 'Austrian',
+    4: 'Burger',
+    5: 'Falafel',
     6: 'Korean',
-    7: 'Steak'
-  };
+    7: 'Steak',
+  }
 
   const freshResult = () => {
-    let result = {};
+    const result = {}
     for (let index in choices) {
-      result[index] = []
+      if ({}.hasOwnProperty.call(choices, index)) {
+        result[index] = []
+      }
     }
-    return result;
-  };
+    return result
+  }
 
-  let result = freshResult();
+  let result = freshResult()
 
   robot.hear(/lunch start/, (res) => {
-    result = freshResult();
+    result = freshResult()
 
-    res.send('Where to go for lunch today? ');
-    res.send('Make your choice e.g. "lunch 1"');
+    res.send('Where to go for lunch today? ')
+    res.send('Make your choice e.g. "lunch 1"')
 
     for (let index in choices) {
-      res.send(`${index}: ${choices[index]}`);
+      res.send(`${index}: ${choices[index]}`)
     }
-  });
+  })
 
   robot.hear(/lunch result/, (res) => {
     for (let index in result) {
-      let members = result[index];
-      if (members.length !== 0)
-        res.send(`${choices[index]}: ${members.length} (${members.join(", ")})`);
+      const members = result[index]
+      if (members.length !== 0) {
+        res.send(`${choices[index]}: ${members.length} (${members.join(', ')})`)
+      }
     }
-  });
+  })
 
-  robot.hear(/lunch ([0-9]+)/, (res, msg) => {
-    let userName = res.message.user.name;
-    let index = res.match[1];
-    let currentUsers = result[index];
+  robot.hear(/lunch ([0-9]+)/, (res) => {
+    const userName = res.message.user.name
+    const index = res.match[1]
+    const currentUsers = result[index]
     if (!currentUsers) {
-      res.reply('You voted for a non-existent choice');
+      res.reply('You voted for a non-existent choice')
     } else if (currentUsers.indexOf(userName) === -1) {
-      currentUsers.push(userName);
+      currentUsers.push(userName)
       res.reply(`You voted for ${choices[index]}`)
     } else {
       res.reply(`You have already voted for ${choices[index]}`)
     }
-  });
-};
+  })
+}
