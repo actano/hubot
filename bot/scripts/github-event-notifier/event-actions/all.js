7897e7b4c2aeb830e48ca56b6b54cf4013d3663e
Hendrik Purmann <h.purmann@googlemail.com>
2016-05-31 18:07:14 +0200

    Fix coding style in all.js

--- bot/scripts/github-event-notifier/event-actions/all.js
+++ bot/scripts/github-event-notifier/event-actions/all.js
@@ -1,17 +1,17 @@
-let push = function(data, callback) {
-  let commit = data.after;
-  let { commits } = data;
-  let { head_commit } = data;
-  let repo = data.repository;
-  let { pusher } = data;
-  let chat_notification = `New Commit \"${head_commit.message}\" to ${repo.full_name} by ${pusher.name}: ${head_commit.url}`;
+function push(data, callback) {
+  const { head_commit } = data
+  const repo = data.repository
+  const { pusher } = data
+  let chatNotification = `New Commit \"${head_commit.message}\" to ${repo.full_name}
+    by ${pusher.name}: ${head_commit.url}`
 
   if (!data.deleted) {
-    if (head_commit.message.indexOf("npm-shrinkwrap.json") !== -1) {
-      chat_notification = chat_notification + "\n\n*Folks please call npm prune and npm install!*";
+    if (head_commit.message.indexOf('npm-shrinkwrap.json') !== -1) {
+      chatNotification = `${chatNotification} \n\n*Folks please call npm prune and npm install!*`
     }
-    return callback(chat_notification);
+    return callback(chatNotification)
   }
-};
+  return undefined
+}
 
-export { push };
+export { push }
