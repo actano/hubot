2dfd4e4f28228a2c8d55244ead89cfa20c1c5885
christian-bick <christian.bick@actano.de>
2016-05-31 13:56:59 +0200

    Migrate lunch poll from coffee to es6

--- bot/scripts/lunch.js
+++ bot/scripts/lunch.js
@@ -1,50 +1,56 @@
-# Description:
-#   Example scripts for you to examine and try out.
-#
-#   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
+module.exports = (robot) => {
 
-module.exports = (robot) ->
+  let choices = {
+    0: "Not hungry",
+    1: "Saigon",
+    2: "Italian",
+    3: "Austrian",
+    4: "Burger",
+    5: "Falafel",
+    6: 'Korean',
+    7: 'Steak'
+  };
 
-    choices =
-        0: "Not hungry"
-        1: "Saigon"
-        2: "Italian"
-        3: "Austrian"
-        4: "Burger"
-        5: "Falafel"
-        6: 'Korean'
-        7: 'Steak'
+  const freshResult = () => {
+    let result = {};
+    for (let index in choices) {
+      result[index] = []
+    }
+    return result;
+  };
 
-    freshResult = ->
-        result = {}
-        for index, choice of choices
-            result[index] = []
-        result
+  let result = freshResult();
 
+  robot.hear(/lunch start/, (res) => {
     result = freshResult();
 
-    robot.hear /lunch start/, (res) ->
-        result = freshResult();
+    res.send('Where to go for lunch today? ');
+    res.send('Make your choice e.g. "lunch 1"');
 
-        res.send "Where to go for lunch today? "
-        res.send "Make your choice e.g. \"lunch 1\""
+    for (let index in choices) {
+      res.send(`${index}: ${choices[index]}`);
+    }
+  });
 
-        for index, choice of choices
-            res.send "#{index}: #{choice}"
+  robot.hear(/lunch result/, (res) => {
+    for (let index in result) {
+      let members = result[index];
+      if (members.length !== 0)
+        res.send(`${choices[index]}: ${members.length} (${members.join(", ")})`);
+    }
+  });
 
-    robot.hear /lunch result/, (res) ->
-        for index, members of result
-            unless members.length == 0
-                res.send "#{choices[index]}: #{members.length} (#{members.join(", ")})"
-
-    robot.hear /lunch ([0-9]+)/, (res, msg) ->
-        userName = res.message.user.name;
-        index = res.match[1];
-        currentUsers = result[index];
-        if not currentUsers?
-            res.reply "You voted for a non-existent choice"
-        else if (userName not in currentUsers)
-            currentUsers.push(userName);
-            res.reply "You voted for #{choices[index]}"
-        else
-            res.reply "You have already voted for #{choices[index]}"
+  robot.hear(/lunch ([0-9]+)/, (res, msg) => {
+    let userName = res.message.user.name;
+    let index = res.match[1];
+    let currentUsers = result[index];
+    if (!currentUsers) {
+      res.reply('You voted for a non-existent choice');
+    } else if (currentUsers.indexOf(userName) === -1) {
+      currentUsers.push(userName);
+      res.reply(`You voted for ${choices[index]}`)
+    } else {
+      res.reply(`You have already voted for ${choices[index]}`)
+    }
+  });
+};
