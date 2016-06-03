f70c2ad791c8730c137d169d462e6d56dac33b31
Hendrik Purmann <h.purmann@googlemail.com>
2016-06-03 15:20:44 +0200

    Add describe to bindings and move to sub-folder

--- bot/scripts/bindings.js
+++ bot/scripts/bindings.js
@@ -1,7 +1,9 @@
 const lunchBot = require('./lunch')
 const githubEvents = require('./github-event-notifier')
+const describe = require('./describe')
 
 module.exports = (robot) => {
   lunchBot(robot)
   githubEvents(robot)
+  describe(robot)
 }
