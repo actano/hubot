a283926073f2d4dafb0892bf3db56b555c313ebb
christian-bick <christian.bick@actano.de>
2016-06-14 17:55:19 +0200

    Bind list command to group-bot

--- bot/scripts/group/index.js
+++ bot/scripts/group/index.js
@@ -5,4 +5,5 @@ module.exports = (robot) => {
   robot.hear(/group open ([a-z][A-Z]+)/i, (res) => bot.open(res))
   robot.hear(/group join ([a-z][A-Z]+)/i, (res) => bot.join(res))
   robot.hear(/group members ([a-z][A-Z]+)/i, (res) => bot.members(res))
+  robot.hear(/group list/i, (res) => bot.list(res))
 }
