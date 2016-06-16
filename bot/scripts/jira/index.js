1f7a452b9e46be4f62bfc4693a2367671a7861db
Pattrick Hueper <phueper@hueper.net>
2016-06-16 09:19:36 +0200

    add tests for the robot hear regex

--- bot/scripts/jira/index.js
+++ bot/scripts/jira/index.js
@@ -1,5 +1,5 @@
 const jira = require('./jira.js')
 
-module.exports = (robot) => {
-  robot.hear(/RX-\d{1,10}/ig, jira.sendIssueLink)
+module.exports = (robot, _jira = jira) => {
+  robot.hear(/RX-\d{1,10}/ig, _jira.sendIssueLink)
 }
--- bot/scripts/jira/jira.ut.js
+++ bot/scripts/jira/jira.ut.js
@@ -8,6 +8,7 @@ chai.use(require('sinon-chai'))
 
 const { sendIssueLink } = require('./jira')
 const messages = require('./messages')
+const hubotScript = require('./index')
 
 describe('jira', () => {
   let res
@@ -27,6 +28,50 @@ describe('jira', () => {
       expect(res.send).to.have.callCount(2)
       expect(res.send).to.have.been.calledWith(messages.issueLink('RX-1234'))
       expect(res.send).to.have.been.calledWith(messages.issueLink('RX-2345'))
+    })
+  })
+})
+
+describe('hubot script', () => {
+  let jira
+  let robot
+
+  beforeEach(() => {
+    jira = {
+      sendIssueLink: sinon.stub(),
+    }
+    robot = {
+      hear: () => {
+      },
+    }
+  })
+
+  describe('hear regex', () => {
+    it('should match a single jira issue reference', () => {
+      sinon.stub(robot, 'hear', (regex) => {
+        const match = 'RX-1234'.match(regex)
+        expect(match).to.eql(['RX-1234'])
+      })
+
+      hubotScript(robot, jira)
+    })
+
+    it('should match multiple jira issue reference', () => {
+      sinon.stub(robot, 'hear', (regex) => {
+        const match = 'RX-1234RX-2345'.match(regex)
+        expect(match).to.eql(['RX-1234', 'RX-2345'])
+      })
+
+      hubotScript(robot, jira)
+    })
+
+    it('should ignore anything but a jira issue reference', () => {
+      sinon.stub(robot, 'hear', (regex) => {
+        const match = 'fooRX-1234bar'.match(regex)
+        expect(match).to.eql(['RX-1234'])
+      })
+
+      hubotScript(robot, jira)
     })
   })
 })
