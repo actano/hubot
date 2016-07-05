e62f297684931ff27a038457dc7233e25922b2b4
christian-bick <christian.bick@actano.de>
2016-07-05 12:52:03 +0200

    Rename commands to be more sexy (use gang instead of groups) and add notify command

--- bot/scripts/group/index.js
+++ bot/scripts/group/index.js
@@ -2,8 +2,9 @@ const { GroupBot } = require('./group-bot')
 
 module.exports = (robot) => {
   const bot = new GroupBot()
-  robot.hear(/group open ([a-z][A-Z]+)/i, (res) => bot.open(res))
-  robot.hear(/group join ([a-z][A-Z]+)/i, (res) => bot.join(res))
-  robot.hear(/group members ([a-z][A-Z]+)/i, (res) => bot.members(res))
-  robot.hear(/group list/i, (res) => bot.list(res))
+  robot.hear(/gang up ([a-z][A-Z]+)/i, (res) => bot.open(res))
+  robot.hear(/gang join ([a-z][A-Z]+)/i, (res) => bot.join(res))
+  robot.hear(/gang members ([a-z][A-Z]+)/i, (res) => bot.members(res))
+  robot.hear(/gang list/i, (res) => bot.list(res))
+  robot.hear(/gang notify ([a-z][A-Z]+) (.+)/i, (res) => bot.notify(res))
 }
