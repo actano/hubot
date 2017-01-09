e97044a68fd52527e709a72a060ca5e7e895ac5c
thrucker <t.rucker@itsonix.eu>
2017-01-09 09:26:42 +0100

    adjust message
    * print correct file path when development `Dockerfile` was updated

--- bot/scripts/github-event-notifier/messages/index.js
+++ bot/scripts/github-event-notifier/messages/index.js
@@ -2,6 +2,6 @@ module.exports = {
   yarnLock: (repoName) =>
     `Updated yarn.lock found at \`${repoName}\`. Please execute \`dyarn install\`.`,
   dockerfile: (repoName) =>
-    `Updated Dockerfile.dev found at \`${repoName}\`. ` +
+    `Updated docker-dev/Dockerfile found at \`${repoName}\`. ` +
     'Please rebuild docker images and containers: `docker-compose down -v && docker-init`.',
 }
