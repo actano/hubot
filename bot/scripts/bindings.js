9299c67606faf8bf4686b04994c0de21aa0f13e9
Lars Menzel <Lars.Menzel@menzelapps.com>
2018-06-12 15:11:09 +0200

    Hackathon Owner Bot: Use correct naming

--- bot/scripts/bindings.js
+++ bot/scripts/bindings.js
@@ -3,13 +3,13 @@ const githubEvents = require('./github-event-notifier')
 const describe = require('./describe')
 const jira = require('./jira')
 const group = require('./group')
-const owner = require('./owner')
+const ownerBot = require('./owner')
 
 module.exports = (robot) => {
   lunchBot(robot)
+  ownerBot(robot)
   jira(robot)
   githubEvents(robot)
   describe(robot)
   group(robot)
-  owner(robot)
 }
